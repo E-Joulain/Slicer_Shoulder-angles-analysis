38c0906d391c02c11d3393ffb8f8fbb0d317d271
E-Joulain <elisejoulain17@gmail.com>
2026-03-20 17:13:19 +0100

    Add files via upload
    Signed-off-by: E-Joulain <elisejoulain17@gmail.com>

--- Beta_angle_ana/Beta_angle_ana.py
+++ Beta_angle_ana/Beta_angle_ana.py
@@ -110,9 +110,20 @@ class Beta_angle_anaWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
             "P2": "Second point on cortical margin of the supraspinatus fossa",
             "P3": "Glenoid lower",
             "P4": "Glenoid upper",
-            "P5": "Lateral acromion",
-            "P6": "Lower acromion",
-            "P7": "Upper femoral head"
+            "P5": "Medial part of the scapular neck",
+
+            "P6": "Lateral acromion",
+            "P7": "Medial acromion",
+            "P8": "Greater tuberosity_head",
+            "P9": "Humeral head contour point 1",
+            "P10": "Humeral head contour point 2",
+            "P11": "Humeral head contour point 3",
+            "P12": "lateral edage of the greater tuberosity",
+
+            "P13": "Proximal point, medial internal cortex",
+            "P14": "Proximal point, lateral internal cortex",
+            "P15": "Distal point, medial internal cortex",
+            "P16": "Distal point, lateral internal cortex"
         }
 
         # Main layout and form
@@ -181,6 +192,7 @@ class Beta_angle_anaWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
         self.fiducialTableLayout.addWidget(self.fiducialTable)
 
+
         # Analysis section
         self.beta_angleGroup = ctk.ctkCollapsibleButton()
         self.beta_angleGroup.text = "Analysis of the image"
@@ -195,6 +207,13 @@ class Beta_angle_anaWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
         self.beta_angleComputeButton = qt.QPushButton("Compute parameters")
         self.beta_angleComputeButton.connect('clicked(bool)', lambda: self.onCalculateMetrics("beta_angle"))
         self.beta_angleLayout.addWidget(self.beta_angleComputeButton)
+
+        # --- Toggle fiducial labels ---
+        self.toggleLabelsButton = qt.QPushButton("Hide/Show Fiducial Labels")
+        self.toggleLabelsButton.setStyleSheet("background-color: green; color: white;")
+        self.toggleLabelsButton.toolTip = "Hide or show all fiducial labels in the scene"
+        self.toggleLabelsButton.connect('clicked(bool)', self.onToggleFiducialLabels)
+        self.beta_angleLayout.addWidget(self.toggleLabelsButton)
 
         self.beta_angleExportButton = qt.QPushButton("Export fiducials nodes")
         self.beta_angleExportButton.connect('clicked(bool)', lambda: self.onExportFiducials("beta_angle"))
@@ -214,6 +233,7 @@ class Beta_angle_anaWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
         # Initialize logic
         self.logic = Beta_angle_anaLogic()
+
 
     # ---------- Callbacks and helper methods ----------
     def onInputVolumeChanged(self, volumeNode):
@@ -235,6 +255,14 @@ class Beta_angle_anaWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
             name = node.GetName() or ""
             if "beta_angle" in name.lower():
                 scene.RemoveNode(node)
+
+        # Remove lines (angles / distances)
+        for node in scene.GetNodesByClass("vtkMRMLMarkupsLineNode"):
+            scene.RemoveNode(node)
+
+        # Remove arcs
+        for node in scene.GetNodesByClass("vtkMRMLMarkupsCurveNode"):
+            scene.RemoveNode(node)
 
     def onPlaceFiducial(self, timepoint: str):
         """Activate placement of fiducials and create the markup node if needed."""
@@ -352,6 +380,17 @@ class Beta_angle_anaWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
         P5 = np.array([points['P5']['x'], points['P5']['y'], points['P5']['z']])
         P6 = np.array([points['P6']['x'], points['P6']['y'], points['P6']['z']])
         P7 = np.array([points['P7']['x'], points['P7']['y'], points['P7']['z']])
+        P8 = np.array([points['P8']['x'], points['P8']['y'], points['P8']['z']])
+        P9 = np.array([points['P9']['x'], points['P9']['y'], points['P9']['z']])
+        P10 = np.array([points['P10']['x'], points['P10']['y'], points['P10']['z']])
+        P11 = np.array([points['P11']['x'], points['P11']['y'], points['P11']['z']])
+        P12 = np.array([points['P12']['x'], points['P12']['y'], points['P12']['z']])
+
+            # inner cortical
+        P13 = np.array([points['P13']['x'], points['P13']['y'], points['P13']['z']])
+        P14 = np.array([points['P14']['x'], points['P14']['y'], points['P14']['z']])
+        P15 = np.array([points['P15']['x'], points['P15']['y'], points['P15']['z']])
+        P16 = np.array([points['P16']['x'], points['P16']['y'], points['P16']['z']])
 
         # --- Beta angle ---
         beta_angle = self.logic.compute_beta_angle(points)
@@ -371,26 +410,161 @@ class Beta_angle_anaWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
             intersect = np.array([px, py, 0.0])
 
         # Draw arc with the intersection as vertex
-        self.logic.draw_arc(intersect, P1, P4, beta_angle, name="BetaAngleArc", color=(1, 1, 0))
+      #  self.logic.draw_arc(intersect, P1, P4, beta_angle, name="BetaAngleArc", color=(1, 1, 0))
 
         # --- Critical Shoulder Angle (CSA) ---
-        csa_angle = self.logic.compute_angle(P4, P3, P5)
-        self.logic.draw_arc(P3, P4, P5, csa_angle, name="CSAArc", color=(0, 1, 0))
+        csa_angle = self.logic.compute_angle(P4, P3, P6)
+        self.logic.draw_line(P3, P4, color=(0, 1, 0), name="CSA")
+        self.logic.draw_line(P4, P6, color=(0, 1, 0))
 
         # --- Subacromial space (SAS) ---
-        sas_distance, closest_point = self.logic.distance_point_to_line(P7, P5, P6)
-        self.logic.draw_line(P7, closest_point, name="SASDistance", color=(0, 0, 1))
+        center, radius = self.logic.compute_circle_center(P9, P10, P11)
+
+        if center is not None:
+            self.logic.draw_circle(center, radius, name="HumeralHeadCircle")
+
+            # Direction of acromial line
+            AB = P7 - P6
+            AB_norm = AB / np.linalg.norm(AB)
+
+            # Normal vector (perpendicular)
+            normal = np.array([-AB_norm[1], AB_norm[0], 0])
+
+            # Signed distance from center to acromial line
+            d = self.logic.distance_point_to_line(center, P6, P7)
+
+            # --- Choose correct side for tangent ---
+            vec = center - P6
+            sign = np.sign(np.dot(vec, normal))
+
+            # Tangent point on circle
+            tangent_point = center - sign * radius * normal
+
+            # Draw tangent line (parallel)
+            tangent_start = tangent_point - AB_norm * 50
+            tangent_end = tangent_point + AB_norm * 50
+            self.logic.draw_line(tangent_start, tangent_end, color=(1, 1, 0), name="SAS_tangent")
+            self.logic.draw_line(P6, P7, color=(1, 1, 0))
+
+            sas_distance = abs(d - radius)
+
+        # --- Glenoid Height (GH) ---
+        gh_distance = np.linalg.norm(P4 - P3)
+      #  self.logic.draw_line(P3, P4, color=(1, 0, 1), name="GH")
+
+        # --- Scapular Neck Length (SNL) ---
+        snl_distance = np.linalg.norm(P5 - P3)
+
+        # --- Scapular Neck Angle (SNA) ---
+        sna_angle = self.logic.compute_angle(P4, P3, P5)
+   #     self.logic.draw_line(P4, P3, color=(1, 0.5, 0), name="SNA")
+   #     self.logic.draw_line(P3, P5, color=(1, 0.5, 0))
+
+
+        # --- Glenoid inclination angle (GIA) ---
+        gia_angle = self.logic.compute_beta_angle({
+            'P1': {'x': P1[0], 'y': P1[1]},
+            'P2': {'x': P2[0], 'y': P2[1]},
+            'P3': {'x': P3[0], 'y': P3[1]},
+            'P4': {'x': P4[0], 'y': P4[1]}
+        })
+        self.logic.draw_line(P1, P2, color=(0.8, 0.2, 1), name="GIA")
+        self.logic.draw_line(P3, P4, color=(0.8, 0.2, 1))
+
+        # --- Distalization shoulder angle (DSA) ---
+        dsa_angle = self.logic.compute_angle(P8, P4, P6)
+        self.logic.draw_line(P8, P4, color=(0.9, 0.4, 0.6), name="DSA")
+        self.logic.draw_line(P4, P6, color=(0.9, 0.4, 0.6))
+
+        # --- Lateralization shoulder angle (LSA) ---
+        lsa_angle = self.logic.compute_angle(P12, P6, P4)
+        self.logic.draw_line(P12, P6, color=(0.9, 0.4, 0.6), name="LSA")
+        self.logic.draw_line(P6, P4, color=(0.9, 0.4, 0.6))
+
+        # --- Humeral Axis ---
+        mid1 = (P13 + P14) / 2
+        mid2 = (P15 + P16) / 2
+        self.logic.draw_line(mid1, mid2, color=(1, 1, 0), name="HumeralAxis")
+        axis_vector = mid2 - mid1
+        axis_unit = axis_vector / np.linalg.norm(axis_vector)
+        axis_length = np.linalg.norm(mid2 - mid1)
+
 
         # Save metrics
         metrics = {
             "beta_angle": beta_angle,
             "critical_shoulder_angle": csa_angle,
-            "subacromial_space": sas_distance
+            "subacromial_space": sas_distance,
+            "GH": gh_distance,
+            "SNL": snl_distance,
+            "SNA": sna_angle,
+            "GIA": gia_angle,
+            "DSA": dsa_angle,
+            "LSA": lsa_angle,
+            "HumeralAxis": axis_vector
         }
         self.logic.saveMetricsToCSV(metrics, patientID, outputDir)
-        slicer.util.showStatusMessage(f"Beta: {beta_angle:.1f}°, CSA: {csa_angle:.1f}°, SAS: {sas_distance:.1f} mm",
-                                      5000)
+        # --- Format results as table ---
+        text = (
+            f"Patient: {patientID}\n"
+            f"-------------------------\n"
+            f"Beta : {beta_angle:.1f}°\n"
+            f"CSA  : {csa_angle:.1f}°\n"
+            f"SAS  : {sas_distance:.1f} mm\n"
+            f"GH   : {gh_distance:.1f} mm\n"
+            f"SNL  : {snl_distance:.1f} mm\n"
+            f"SNA  : {sna_angle:.1f}°\n"
+            f"GIA  : {gia_angle:.1f}°\n"
+            f"DSA  : {dsa_angle:.1f}°\n"
+            f"LSA  : {lsa_angle:.1f}°\n"
+        )
 
+
+        # --- Display in slice views ---
+        layoutManager = slicer.app.layoutManager()
+
+        for viewName in ["Red", "Yellow", "Green"]:
+            sliceWidget = layoutManager.sliceWidget(viewName)
+            if sliceWidget:
+                sliceView = sliceWidget.sliceView()
+                cornerAnnotation = sliceView.cornerAnnotation()
+                cornerAnnotation.SetText(vtk.vtkCornerAnnotation.UpperLeft, text)
+                sliceView.render()
+
+        # --- FORCE HIDE LABELS ---
+
+    def onToggleFiducialLabels(self):
+        """Toggle fiducial labels visibility."""
+        # Check first node to decide current state
+        scene = slicer.mrmlScene
+        fidNodes = scene.GetNodesByClass("vtkMRMLMarkupsFiducialNode")
+        if not fidNodes:
+            slicer.util.showStatusMessage("No fiducial nodes in the scene.", 2000)
+            return
+
+        displayNode = fidNodes.GetItemAsObject(0).GetDisplayNode()
+        if not displayNode:
+            return
+
+        currentVisibility = displayNode.GetPointLabelsVisibility()
+        newVisibility = not currentVisibility
+
+        self.setFiducialLabelsVisible(newVisibility)
+
+        if newVisibility:
+            slicer.util.showStatusMessage("Fiducial labels shown.", 2000)
+        else:
+            slicer.util.showStatusMessage("Fiducial labels hidden.", 2000)
+
+    def setFiducialLabelsVisible(self, visible: bool = True):
+        """Show or hide all fiducial labels in the scene."""
+        scene = slicer.mrmlScene
+        for node in scene.GetNodesByClass("vtkMRMLMarkupsFiducialNode"):
+            displayNode = node.GetDisplayNode()
+            if displayNode:
+                displayNode.SetPointLabelsVisibility(visible)
+                displayNode.SetPropertiesLabelVisibility(visible)
+                node.Modified()
 
 class Beta_angle_anaLogic(ScriptedLoadableModuleLogic):
     """Logic class for data processing, angle computation, and visualization."""
@@ -488,7 +662,8 @@ class Beta_angle_anaLogic(ScriptedLoadableModuleLogic):
         proj = np.dot(point_vec, line_vec) / line_len
         closest = A + proj * line_vec / line_len
         dist = np.linalg.norm(P - closest)
-        return dist, closest
+        return dist #, closest
+
 
     # --- Generate points along an arc ---
     def generate_arc_points(self, vertex, p1, p2, angle_deg, num_points=30):
@@ -510,7 +685,7 @@ class Beta_angle_anaLogic(ScriptedLoadableModuleLogic):
         return points
 
     # --- Draw arc ---
-    def draw_arc(self, vertex, p1, p2, angle_deg, name="Arc", color=(1, 1, 0), thickness=0.0001, num_points=30):
+    def draw_arc(self, vertex, p1, p2, angle_deg, name="Arc", color=(1, 1, 0), thickness=0.00001, num_points=30):
         """
         Draw an arc centered at `vertex`, starting at `p1` and spanning `angle_deg` toward `p2`.
         The arc is drawn in the XY plane.
@@ -542,6 +717,7 @@ class Beta_angle_anaLogic(ScriptedLoadableModuleLogic):
 
         # Display properties
         displayNode = curveNode.GetDisplayNode()
+
         if displayNode:
             displayNode.SetSelectedColor(*color)
             displayNode.SetLineThickness(thickness)
@@ -549,22 +725,118 @@ class Beta_angle_anaLogic(ScriptedLoadableModuleLogic):
             displayNode.SetVisibility3D(True)
 
     # --- Draw line ---
-    def draw_line(self, A, B, name="Line", color=(1, 0, 0), thickness=0.5):
+    def draw_line(self, A, B, color=(1, 0, 0), thickness=0.2, name = None):
         """Draw a line as a Markups Line Node."""
         # Remove previous line node with same name
         for node in slicer.mrmlScene.GetNodesByClass("vtkMRMLMarkupsLineNode"):
             if node.GetName() == name:
                 slicer.mrmlScene.RemoveNode(node)
 
-        lineNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsLineNode", name)
+        lineNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsLineNode") #, name
         lineNode.AddControlPoint(A.tolist())
         lineNode.AddControlPoint(B.tolist())
 
         displayNode = lineNode.GetDisplayNode()
+
         if displayNode:
             displayNode.SetSelectedColor(*color)
             displayNode.SetLineThickness(thickness)
             displayNode.SetVisibility2D(True)
             displayNode.SetVisibility3D(True)
 
+            displayNode.SetPointLabelsVisibility(False)
+            displayNode.SetPropertiesLabelVisibility(False)
 
+            lineNode.SetName("")
+
+            lineNode.SetNthControlPointLabel(0, "")
+            lineNode.SetNthControlPointLabel(1, "")
+
+            if name:
+                lineNode.SetName(name)
+
+                # show only the custom label
+                displayNode.SetPointLabelsVisibility(True)
+                displayNode.SetPropertiesLabelVisibility(False)
+
+                lineNode.SetNthControlPointLabel(0, name)
+                lineNode.SetNthControlPointLabel(1, "")
+
+    # --- Calculate the center of the circle with 3 reference point to find the center of rotation of the humeral head ---
+    def compute_circle_center(self, A, B, C):
+        """
+        Compute circle center passing through 3 points (2D).
+        """
+        ax, ay = A[:2]
+        bx, by = B[:2]
+        cx, cy = C[:2]
+
+        d = 2 * (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by))
+
+        if abs(d) < 1e-6:
+            return None, None
+
+        ux = ((ax ** 2 + ay ** 2) * (by - cy) +
+              (bx ** 2 + by ** 2) * (cy - ay) +
+              (cx ** 2 + cy ** 2) * (ay - by)) / d
+
+        uy = ((ax ** 2 + ay ** 2) * (cx - bx) +
+              (bx ** 2 + by ** 2) * (ax - cx) +
+              (cx ** 2 + cy ** 2) * (bx - ax)) / d
+
+        center = np.array([ux, uy, A[2]])
+        radius = np.linalg.norm(center - A)
+
+        return center, radius
+
+    def draw_circle(self, center, radius, name="Circle", color=(0, 1, 1), num_points=18):
+
+        import numpy as np
+
+        for node in slicer.mrmlScene.GetNodesByClass("vtkMRMLMarkupsCurveNode"):
+            if node.GetName() == name:
+                slicer.mrmlScene.RemoveNode(node)
+
+        curveNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsCurveNode", name)
+
+        for t in np.linspace(0, 2 * np.pi, num_points):
+            x = center[0] + radius * np.cos(t)
+            y = center[1] + radius * np.sin(t)
+            curveNode.AddControlPoint([x, y, center[2]])
+
+        displayNode = curveNode.GetDisplayNode()
+        displayNode.SetSelectedColor(*color)
+        displayNode.SetLineThickness(0.002)
+
+    # --- Compute the tangent to a circle ---
+    def compute_tangent_at_point(self, center, point):
+        """
+        Compute tangent line at a point of a circle.
+        Returns two points defining the tangent.
+        """
+
+        radius_vec = point - center
+
+        # perpendicular vector (2D)
+        tangent_vec = np.array([-radius_vec[1], radius_vec[0], 0])
+
+        tangent_vec = tangent_vec / np.linalg.norm(tangent_vec)
+
+        p1 = point - 50 * tangent_vec
+        p2 = point + 50 * tangent_vec
+
+        return p1, p2
+
+    def draw_parallel_tangent(self, center, radius, A, B, name="SAS_Tangent"):
+
+        AB = B - A
+
+        normal = np.array([-AB[1], AB[0], 0])
+        normal = normal / np.linalg.norm(normal)
+
+        tangent_point = center + radius * normal
+
+        p1 = tangent_point - 50 * AB / np.linalg.norm(AB)
+        p2 = tangent_point + 50 * AB / np.linalg.norm(AB)
+
+        self.draw_line(p1, p2, name=name, color=(0, 0, 1))
